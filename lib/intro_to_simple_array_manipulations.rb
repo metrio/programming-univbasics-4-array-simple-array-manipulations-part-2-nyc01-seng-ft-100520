1f190f551a9186cb64ae14e3f9a0bae2db2fbe64
Demetrio Lima <dlima.nyit@gmail.com>
2020-09-17 16:19:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,8 +4,6 @@ def using_concat(my_favorite_things, more_favs)
 end
 
 def using_insert(list_of_programming_languages, another_language)
-  new_array = []
-  new_array << list_of_programming_languages.insert
-  binding.pry
+  list_of_programming_languages.insert(4, another_language)
 end
 
